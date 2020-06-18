97fa916f3f0ea9e89746c6e36b59393290281fb8
chtti <chtti@cht.com.tw>
2020-06-18 16:20:42 +0800

    More cases for CRUD Repository

--- src/main/java/com/chtti/springboot/demo/Demo7JPADocker/BeverageRunner1.java
+++ src/main/java/com/chtti/springboot/demo/Demo7JPADocker/BeverageRunner1.java
@@ -5,6 +5,9 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -16,19 +19,44 @@ public class BeverageRunner1 implements CommandLineRunner {
     public void run(String... args) throws Exception {
         LOGGER.info("start to test beverage repository");
         insertSomeData();
+        loadSomeData();
         loadAllData();
+        loadDataByPage();
         deleteAndCheckCount();
     }
 
     private void loadAllData() {
+        LOGGER.info("find all data");
         repository.findAll().forEach(beverage -> LOGGER.info("get a record==>{}",beverage));
     }
 
+    private void loadSomeData(){
+
+        LOGGER.info("find all american only");
+        repository.findMatchByTitle("americano").forEach(beverage -> LOGGER.info("aanericano only == >{}",beverage));
+    }
+
+    private void loadDataByPage(){
+        LOGGER.info("load data by page");
+        PageRequest pr1 = PageRequest.of(0,4);
+        repository.findAll(pr1).forEach(beverage -> {
+            LOGGER.info("#{}  page:{}",pr1.getPageNumber(),beverage);
+        });
+        Pageable pr2 = pr1.next();
+        repository.findAll(pr2).forEach(beverage -> {
+            LOGGER.info("#{}  page:{}",pr2.getPageNumber(),beverage);
+        });
+    }
+
     private void insertSomeData() {
-        repository.save(new
-                Beverage("americano","black coffee with no milk"));
-        repository.save(new Beverage("latte", "espresso with 60% milk"));
-        repository.save(new Beverage("Assam black tea", "normal black tea"));
+        repository.save(new Beverage("americano","ice black coffee with no milk"));
+        repository.save(new Beverage("americano","hot black coffee with no milk"));
+        repository.save(new Beverage("ice latte", "espresso with 60% milk"));
+        repository.save(new Beverage("hot latte", "espresso with 60% milk"));
+        repository.save(new Beverage("ice Assam black tea", "normal black tea"));
+        repository.save(new Beverage("hot Assam black tea", "normal black tea"));
+        repository.save(new Beverage("ice milk tea", "balck tea with milk"));
+        repository.save(new Beverage("hot milk tea", "balck tea with milk"));
     }
 
     private void deleteAndCheckCount(){
--- src/main/java/com/chtti/springboot/demo/Demo7JPADocker/MyBeverageCRUDRepository.java
+++ src/main/java/com/chtti/springboot/demo/Demo7JPADocker/MyBeverageCRUDRepository.java
@@ -1,7 +1,15 @@
 package com.chtti.springboot.demo.Demo7JPADocker;
 
 import com.chtti.springboot.demo.Demo7JPADocker.beans.Beverage;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface MyBeverageCRUDRepository extends CrudRepository<Beverage,Long> {
+
+    public List<Beverage> findMatchByTitle(String title);
+    Page<Beverage> findAll(Pageable pageable);
+
 }
