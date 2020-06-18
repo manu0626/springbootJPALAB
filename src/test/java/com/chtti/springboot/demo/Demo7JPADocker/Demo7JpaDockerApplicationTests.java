18e57a2715d89128cbf9654547a2e0af121d8b18
chtti <chtti@cht.com.tw>
2020-06-18 13:52:01 +0800

    Initial Project setup from start.spring.io

---
+++ src/test/java/com/chtti/springboot/demo/Demo7JPADocker/Demo7JpaDockerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chtti.springboot.demo.Demo7JPADocker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Demo7JpaDockerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
